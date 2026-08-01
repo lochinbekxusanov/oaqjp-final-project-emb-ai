0accc05a71b973802764171e34fa416d46cb19d8
Lochinbek Xusanov <lochinbekxusanov@gmail.com>
2026-08-01 15:31:38 -0400

    Task 7: Add error handling for blank input

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,23 +3,33 @@ import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
+
     myobj = { "raw_document": { "text": text_to_analyze } }
-    
+
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
+
     response = requests.post(url, json=myobj, headers=header)
-    
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     formatted_response = json.loads(response.text)
-    
+
     emotions = formatted_response['emotionPredictions'][0]['emotion']
-    
+
     anger = emotions['anger']
     disgust = emotions['disgust']
     fear = emotions['fear']
     joy = emotions['joy']
     sadness = emotions['sadness']
-    
+
     emotion_scores = {
         'anger': anger,
         'disgust': disgust,
@@ -27,9 +37,9 @@ def emotion_detector(text_to_analyze):
         'joy': joy,
         'sadness': sadness
     }
-    
+
     dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-    
+
     final_output = {
         'anger': anger,
         'disgust': disgust,
@@ -38,5 +48,5 @@ def emotion_detector(text_to_analyze):
         'sadness': sadness,
         'dominant_emotion': dominant_emotion
     }
-    
+
     return final_output
